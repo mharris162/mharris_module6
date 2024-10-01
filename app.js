a9abd6e7393161154537ebd5ea3485e9e1513d09
mharris162 <mharris162@student.alamo.edu>
2024-10-01 18:54:01 -0500

    array

---
+++ app.js
@@ -0,0 +1,35 @@
+// object
+
+//object method
+
+//array
+
+const bday = ["March 30", 'June 6', 'January 4', 'August 8', 'July 8','November 11'];// array listed 
+console.log(bday); // recalling full array to console
+
+console.log(bday[4]); // recalling item 4 in array to console
+
+
+
+bday.push('July 31'); //adding to array
+console.log(bday); // recalling full array to console
+
+bday.pop();// removing last from array
+console.log(bday); // recalling full array to console
+
+
+
+bday.shift(); // remove first from array
+console.log(bday);// recalling full array to console
+
+bday.unshift('April 14'); // add to beginning of array
+console.log(bday);// recalling full array to console
+
+bday.indexOf('November 11'); // recalling item list number in array
+console.log(bday.indexOf('November 11'));
+
+ 
+
+//array method
+
+
